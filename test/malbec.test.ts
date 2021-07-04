8068d9eb3efa920c63624eea7a36bef29e4ed7d9
Jared Franzone <jaredfranzone@gmail.com>
2021-07-03 19:21:09 -0700

    Initial commit

---
+++ test/malbec.test.ts
@@ -0,0 +1,13 @@
+import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import * as cdk from '@aws-cdk/core';
+import * as Malbec from '../lib/malbec-stack';
+
+test('Empty Stack', () => {
+    const app = new cdk.App();
+    // WHEN
+    const stack = new Malbec.MalbecStack(app, 'MyTestStack');
+    // THEN
+    expectCDK(stack).to(matchTemplate({
+      "Resources": {}
+    }, MatchStyle.EXACT))
+});
